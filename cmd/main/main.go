d3a8ae79833b1733edcd11868112e6f105793706
Pamela Silva <silvamrt6@gmail.com>
2022-12-19 23:02:23 -0300

    finalizado

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/PamelaMarinho/go-bookstore/pkg/routes"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	fmt.Println(http.ListenAndServe("localhost:8080", r))
+}
